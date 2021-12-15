2d6fdf7c3a590dccd46a6c0cdcfb44853842591c
Olesia <olesyao2003@gmail.com>
2021-12-15 12:15:01 +0200

    create csv file

--- olesia.py
+++ olesia.py
@@ -1,1 +1,9 @@
-print('Hello')
+import pandas as pd
+
+
+def create_csv_file(path):
+    data = {'Data': [], 'Goal': [], 'Number of time(goal)': [], 'Number of time(reality)': [
+    ], 'Success': [], 'Failure': []}
+    df = pd.DataFrame(data, columns=[
+                      'Data', 'Goal', 'Number of time(goal)', 'Number of time(reality)', 'Success', 'Failure'])
+    return df
